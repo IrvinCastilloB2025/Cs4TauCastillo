86cbd216f94485be29edf215bb4904ce57b391e1
IrvinCastilloB2025 <112992354+IrvinCastilloB2025@users.noreply.github.com>
2022-09-07 20:22:33 +0800

    Create Q1Ex1.java

---
+++ Q1Ex1/Q1Ex1.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package castillo_testproject;
+import java.util.Scanner;
+
+/**
+ *
+ * @author TAU
+ */
+public class Castillo_TestProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //header
+        System.out.print("List of oc's I may or may not regret making\n\n");
+        
+        
+        //character 1
+        String Name1 = "Zane";
+        int Year1 = 2015;
+        int Rating1 = 1;
+        //char1 code
+        System.out.print("Character 1\n");
+        System.out.printf("Name: %s\n", Name1);
+        System.out.printf("Year Created: %d\n", Year1);
+        System.out.printf("Creator Rating: %d/10\n\n", Rating1);
+        
+        
+        //character 2
+        String Name2 = "Sam";
+        int Year2 = 2019;
+        int Rating2 = 9;        
+        //char2 code
+        System.out.print("Character 2\n");
+        System.out.printf("Name: %s\n", Name2);
+        System.out.printf("Year Created: %d\n", Year2);
+        System.out.printf("Creator Rating: %d/10\n\n", Rating2);
+        
+        
+        //character 3
+        String Name3 = "Unnamed";
+        int Year3 = 2022;
+        int Rating3 = 8;           
+        //char3 code
+        System.out.print("Character 3\n");
+        System.out.printf("Name: %s\n", Name3);
+        System.out.printf("Year Created: %d\n", Year3);
+        System.out.printf("Creator Rating: %d/10\n\n", Rating3);
+    }
+    
+}
